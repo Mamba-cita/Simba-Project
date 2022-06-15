a14661d105c33cc835ee048f8674f3b7b9c8bd72
Mamba-Cita <edwnsjohn52@gmail.cm>
2022-06-15 22:50:05 +0300

    added login form in loginpage dr

--- client/src/components/pages/HomePage/Home.js
+++ client/src/components/pages/HomePage/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../MainNav/Navbar";
+import Login from "../LogInPage/Login";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Login/>
     </div>
   );
 }
